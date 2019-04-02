3d69b63a99fc611427be8246a364a8629032aa0a
clueless <clueless@king.lan>
2019-04-01 22:28:44 -0700

    new template

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/clueless/Documents/jhu/software_engineering/clue-less/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,23 +1,22 @@
-# Copyright 2015 Google Inc. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""mysite URL Configuration
 
-from django.conf.urls import include, url
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-
-from polls.views import index
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^$', index),
-    url(r'^admin/', include(admin.site.urls)),
+    path('', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
